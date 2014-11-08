59f97af2916d9374c1bb628397a207f3e2a776bb
jwncoexists <jenniferwnelson@sbcglobal.net>
2014-11-07 23:15:01 -0800

    Added feature to pause timer.

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -2,93 +2,40 @@ app = angular.module('pomodoroApp');
 
 app.controller('HomeCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
   console.log('In HomeCtrl');
-  $scope.test = "hello world";
   $scope.timers = {
     curTimerObj: null,
     workTimer: { 
       name: "work", 
-      duration: 1500000, 
+      length: 1500, 
       startTime: 0, 
-      stopTime: 0, 
+      stopTime: 0,
+      duration: 0, 
+      paused: false,
       soundPath: "/assets/sounds/work",
       imageFile: "/assets/images/work.jpg" 
     },
     breakTimer: { 
       name: "break", 
-      duration: 300000, 
+      length: 300, 
       startTime: 0, 
-      stopTime: 0, 
+      stopTime: 0,
+      duration: 0,
+      paused: false,
       soundPath: "/assets/sounds/break",
       imageFile: "/assets/images/break.jpg" }
   };
   
+  $scope.timers.workTimer.sound = new buzz.sound( $scope.timers.workTimer.soundPath, 
+  {
+      formats: ['wav'],
+      preload: true
+  });
   $scope.timers.breakTimer.sound = new buzz.sound( $scope.timers.breakTimer.soundPath, 
   {
       formats: ['wav'],
       preload: true
   });
 
-  $scope.whichView = function() {
-    var view = "work";
-    if($scope.timers.curTimerObj)
-    {
-      if($scope.timers.curTimerObj.name == "break")
-      {
-        view = "break";
-      }
-    }
-    return (view);
-  }
-  $scope.timers.workTimer.sound = new buzz.sound( $scope.timers.workTimer.soundPath, 
-  {
-      formats: ['wav'],
-      preload: true
-  });
-
-  $scope.startTimer = function(timerObj) {
-    timerObj.startTime = moment();
-    timerObj.stopTime = timerObj.startTime;
-    $scope.timers.curTimerObj = timerObj;
-  };
-
-  $scope.stopTimer = function(timerObj) {
-    timerObj.stopTime = moment();
-    $scope.timers.curTimerObj = null;
-    $interval.cancel($scope.timerInterval);
-  };
-
-  $scope.switchTimers = function() {
-    var nextTimerObj = $scope.timers.workTimer;
-    if ($scope.timers.curTimerObj) {
-      if ($scope.timers.curTimerObj.name == "work") {
-        nextTimerObj = $scope.timers.breakTimer;
-      }
-      stopTimer($scope.timers.curTimerObj);
-    }
-    $scope.startTimer(nextTimerObj);
-  };
-
-  $scope.timeRemaining = function() {
-    var remaining = 0;
-    if ($scope.timers.curTimerObj) {
-      var curTimer = $scope.timers.curTimerObj;
-      if (curTimer.startTime > 0) {
-        console.log("duration: " + curTimer.duration);
-        console.log("start time: " + curTimer.startTime);
-        remaining = (curTimer.duration - (moment() - moment(curTimer.startTime)))/1000;
-      }
-    }
-    return remaining;
-  };
-
-  $scope.checkCurTimer = function() {
-    if ($scope.timers.curTimerObj) {
-      if ($scope.timers.curTimerObj.startTime > 0 && $scope.timeRemaining() <= 0) {
-        $scope.timers.curTimerObj.sound.play();
-        $scope.stopTimer($scope.timers.curTimerObj);
-      }
-    } 
-  }; // checkCurTimer
 
   $scope.launchInterval = function() {
     console.log('launching work interval');
@@ -98,9 +45,93 @@ app.controller('HomeCtrl', ['$scope', '$http', '$interval', function($scope, $ht
      }, 1000);
   };
 
+  $scope.startTimer = function(timerObj) {
+    if (timerObj.paused) {
+
+    }
+    else {
+      timerObj.startTime = moment();
+      timerObj.stopTime = timerObj.startTime;
+      timerObj.duration = timerObj.length;
+    }
+    timerObj.paused = false;
+    $scope.timers.curTimerObj = timerObj;
+    console.log('Initializing timer to: ' + $scope.timers.curTimerObj.duration );
+    $scope.startPauseText = "Pause Timer";
+    $scope.launchInterval();
+  };
+
+  $scope.resetTimer = function(timerObj) {
+    timerObj.startTime = moment();
+    timerObj.stopTime = timerObj.startTime;
+    timerObj.duration = 0;
+    timerObj.paused = false;
+    $scope.timers.curTimerObj = timerObj;
+    $scope.startPauseText = "Pause Timer";
+  };
+
+  $scope.stopTimer = function(timerObj) {
+    timerObj.stopTime = moment();
+    timerObj.duration = 0;
+    timerObj.paused = true;
+    $scope.timers.curTimerObj = null;
+    $interval.cancel($scope.timerInterval);
+    $scope.startPauseText = "Start Timer";
+  };
+
+  $scope.pauseTimer = function(timerObj) {
+    console.log('Pausing timer');
+    timerObj.stopTime = moment();
+    $interval.cancel($scope.timerInterval);
+    timerObj.paused = true;
+    $scope.startPauseText = "Start Timer";
+  };
+
+  $scope.switchTimers = function(timerObj) {
+    console.log('in Switch Timers')
+    var nextTimerObj = $scope.timers.workTimer;
+    if ($scope.timers.curTimerObj) {
+      $scope.stopTimer($scope.timers.curTimerObj);
+    }
+    $scope.startTimer(timerObj);
+    $scope.curView = timerObj.name;
+  };
+
+  $scope.startPauseTimer = function(timerObj) {
+    console.log('in Start/Pause Timer')
+    // if we have a currentTimerObj, then timer is running
+    if (timerObj.paused) {
+      $scope.startTimer(timerObj);
+      
+    }
+    else {
+      $scope.pauseTimer(timerObj);
+    }
+
+  };
+
+  $scope.timeRemaining = function() {
+    var remaining = 0;
+    if ($scope.timers.curTimerObj) {
+      remaining = $scope.timers.curTimerObj.duration;
+    }
+    return remaining;
+  };
+
+  $scope.checkCurTimer = function() {
+    if ($scope.timers.curTimerObj) {
+      $scope.timers.curTimerObj.duration--;
+      if ($scope.timers.curTimerObj.startTime > 0 && 
+          $scope.timers.curTimerObj.duration <= 0) {
+        $scope.timers.curTimerObj.sound.play();
+        $scope.stopTimer($scope.timers.curTimerObj);
+      }
+    } 
+  }; // checkCurTimer
+
+
   var init = function() {
-    $scope.switchTimers();
-    $scope.timerInterval = $scope.launchInterval();
+    $scope.switchTimers($scope.timers.workTimer);
   }
 
   init();
